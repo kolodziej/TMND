a904f6c212efdbb2be23df23200de6952c59824b
Kacper Kołodziej <kacper@kolodziej.in>
2015-06-01 19:34:50 +0200

    implementation of Socks5::Request (mainly function to_buffer) implementation of Socks5::Socks5 functions

---
+++ src/socks5/socks5.cpp
@@ -0,0 +1,86 @@
+#include "tmnd/socks5/socks5.hpp"
+
+namespace tmnd { namespace socks5 {
+
+Socks5::Socks5(boost::asio::ip::tcp::socket& socket) :
+  socket_(socket)
+{}
+
+void Socks5::initialize(std::initializer_list<auth_method> methods)
+{
+  init_request_.nmethods = methods.length();
+  for (int i = 0; i < methods.length(); ++i)
+  {
+    init_request_.methods[i] = static_cast<uint8_t>(method);
+  }
+
+  using namespace std::placeholders;
+  socket_.async_send(
+      boost::asio::buffer(reinterpret_cast<char*>(&init_request_), init_request_.nmethods + 2),
+      std::bind(&Socks5::initialize_callback, this, _1, _2)
+  );
+}
+
+void Socks5::initialize_callback(boost::system::error_code ec, size_t transferred)
+{
+  if (ec.value() != 0)
+  {
+    // log error
+  } else
+  {
+    initialize_reply();
+  }
+}
+
+void Socks5::initialize_reply()
+{
+  using namespace std::placeholders;
+  socket_.async_receive(
+      boost::asio::buffer(reinterpret_cast<char*>(&init_reply_), sizeof(init_reply_)),
+      std::bind(&Socks5::initialize_reply_callback, this, _1, _2)
+  );
+}
+
+void Socks5::initialize_reply_callback(boost::system::error_code ec, size_t transferred)
+{
+  if (ec.value() != 0)
+  {
+    // log error
+  } else
+  {
+    authenticate(init_reply_.method);
+  }
+}
+
+void Socks5::authenticate(auth_method method)
+{
+  switch (method)
+  {
+    case auth_method::no_authentication:
+      no_authentication();
+      break;
+
+    case auth_method::gssapi:
+      gssapi();
+      break;
+
+    case auth_method::user_pass:
+      user_pass();
+      break;
+      
+    case auth_methods::no_methods:
+      // log error
+      break;
+
+    default:
+      // log error
+      break;
+  }
+}
+
+void Socks5::request(command cmd, address_type atype, std::string address)
+{
+  
+}
+
+} }
