730f3482400871a126a77d6e4fa2fdb6aa3dd4c9
Kacper Kołodziej <kacper@kolodziej.in>
2015-07-19 22:52:38 +0200

    added Message class which represents all available TMND messages

---
+++ include/tmnd/message.hpp
@@ -0,0 +1,31 @@
+#ifndef TMND_MESSAGE_HPP
+#define TMND_MESSAGE_HPP
+#include <cstdint>
+#include <boost/asio.hpp>
+
+#include "message_type.hpp"
+
+namespace tmnd {
+
+class Message
+{
+ public:
+  Message(uint8_t version, MessageType type, std::string data = std::string());
+
+  uint16_t version() const;
+  MessageType type() const;
+  uint32_t size() const;
+
+  std::string data() const;
+
+  boost::asio::mutable_buffer buffer();
+
+ private:
+  uint16_t header_;
+  uint32_t size_;
+  std::string data_;
+};
+
+}
+
+#endif
