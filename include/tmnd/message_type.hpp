318b3803a2cd3e53fd2c2596c6ed5e5285537751
Kacper Kołodziej <kacper@kolodziej.in>
2015-07-19 22:39:09 +0200

    added tmnd::MessagType enum class

---
+++ include/tmnd/message_type.hpp
@@ -0,0 +1,24 @@
+#ifndef TMND_MESSAGE_TYPE_HPP
+#define TMND_MESSAGE_TYPE_HPP
+#include <cstdint>
+
+namespace tmnd {
+
+enum class MessageType : uint8_t
+{
+  Hello = 0x00,
+  Ping = 0x01,
+  Pong = 0x02,
+  Invite = 0x03,
+  Accept = 0x04,
+  Reject = 0x05,
+  Text = 0x06,
+  FileStart = 0x07,
+  FileData = 0x08,
+  FileEnd = 0x09,
+  Contact = 0x0A,
+  NewHostname = 0x0B,
+  Bye = 0xFF
+};
+
+#endif
